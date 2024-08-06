147321975616d209d7a9980b95d753bebaa2acc7
jlcarreno <jlcarreno.c@gmail.com>
2024-08-06 22:27:33 +0000

    actualizar

--- Edad100.java
+++ Edad100.java
@@ -5,7 +5,7 @@ public class Edad100 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Ingrese su nombre; ");
-            String nombre= scanner.nextLine();
+            String nombre = scanner.nextLine();
 
             System.out.print("Ingrese su edad: ");
             int edad = scanner.nextInt();
@@ -13,7 +13,10 @@ public class Edad100 {
             int anioActual = Year.now().getValue();
             int anio100 = anioActual + (100 - edad);
 
+            int aniorestante = (100 - edad);
+
             System.out.println(nombre + ", tendras 100 años en el año " + anio100);
+            System.out.println(nombre + ", te faltan " + aniorestante + " años para cumplir 100 años"); 
         }
     }
 }
