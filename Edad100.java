098cb01bd77dd460d7f15dd2af4f9da619665738
jlcarreno <jlcarreno.c@gmail.com>
2024-08-06 20:33:32 +0000

    Edad100

---
+++ Edad100.java
@@ -0,0 +1,19 @@
+import java.time.Year;
+import java.util.Scanner;
+
+public class Edad100 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Ingrese su nombre; ");
+            String nombre= scanner.nextLine();
+
+            System.out.print("Ingrese su edad: ");
+            int edad = scanner.nextInt();
+
+            int anioActual = Year.now().getValue();
+            int anio100 = anioActual + (100 - edad);
+
+            System.out.println(nombre + ", tendras 100 años en el año " + anio100);
+        }
+    }
+}
---
+++ HolaJose.java
@@ -0,0 +1,5 @@
+public class HolaJose {
+    public static void main(String[] args) {
+        System.out.println("¡¡Hola, Jose Luis Carreño!!");
+    }
+}
