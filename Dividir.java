2116e8041272130074684ec4e055a88b5d15c5bc
jlcarreno <jlcarreno.c@gmail.com>
2024-08-07 18:33:09 +0000

    division

--- Dividir.java
+++ Dividir.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Multiplicar {
+public class Dividir {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String respuesta;
         do {
-            System.out.print("Ingrese un número para ver su tabla de multiplicar: ");
+            System.out.print("Ingrese un número para ver su tabla de división: ");
 
             int numero = scanner.nextInt();
 
-            System.out.println("Tabla de multiplicar del " + numero + ":");
+            System.out.println("Tabla de dividir del " + numero + ":");
             for (int i = 1; i <= 10; i++) {
-                System.out.println(numero + " x " + i + " = " + (numero * i));
+                System.out.println(i + " / " + numero + " = " + (i / numero));
             }
             System.out.println("¿Quiere ver otra tabla? (S/N): ");
             respuesta = scanner.next();
