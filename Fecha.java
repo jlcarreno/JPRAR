e88d570b8721dc842ce707e7e964f4d61f5602f6
jlcarreno <jlcarreno.c@gmail.com>
2024-08-07 02:21:29 +0000

    actual

---
+++ Fecha.java
@@ -0,0 +1,10 @@
+import javax.xml.crypto.Data;
+
+public class Fecha {
+    public static void main(String[] args) {
+        int Fecha = Data.now().getValue();
+        int fechahoy = Fecha
+
+        System.out.println(fechahoy);
+    }
+}
---
+++ Multiplicar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Multiplicar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String respuesta;
+        do {
+            System.out.print("Ingrese un número para ver su tabla de multiplicar: ");
+
+            int numero = scanner.nextInt();
+
+            System.out.println("Tabla de multiplicar del " + numero + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numero + " x " + i + (numero * i));
+            }
+            System.out.println("¿Quiere ver otra tabla? (S/N): ");
+        } while (respuesta.equalsIgnoreCase("S"));
+                    System.out.println("¡Gracias por usar el programa!");
+                    scanner.close();
+        }
+    }
